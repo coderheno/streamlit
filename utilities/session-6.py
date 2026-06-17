20baaa298247990353c672fba6b84d8b9fa2dc94
coderheno <vijayarputh@gmail.com>
2026-06-17 15:18:30 +0530

    Update session-6.py

--- utilities/session-6.py
+++ utilities/session-6.py
@@ -77,7 +77,7 @@ if module == "🏠 Home":
     st.write("")
 
     st.image(
-        "https://www.freecodecamp.org/news/content/images/2022/06/js.png",
+        "https://images.unsplash.com/photo-1581091870620-3c7f1e5b8f4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHx8&w=1000&q=80",
         width=250
     )
 
