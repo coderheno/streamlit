1058f0f769a41699eab59ec7e32fb48c7ed72cf0
coderheno <vijayarputh@gmail.com>
2026-06-17 15:19:07 +0530

    Update session-6.py

--- utilities/session-6.py
+++ utilities/session-6.py
@@ -76,10 +76,7 @@ if module == "🏠 Home":
 
     st.write("")
 
-    st.image(
-        "https://images.unsplash.com/photo-1581091870620-3c7f1e5b8f4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHx8&w=1000&q=80",
-        width=250
-    )
+  
 
     st.write("")
 
